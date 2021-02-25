683e6d954acfb611d4a6326819ced9dbb08150b8
grigo <grigo@LAPTOP-TKQJP1NQ>
2021-02-25 17:18:06 +0200

    Add clase

---
+++ seminar1/src/clase/Animal.java
@@ -0,0 +1,18 @@
+package clase;
+
+public class Animal {
+  private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public Animal(String name) {
+	super();
+	this.name = name;
+}
+}
--- seminar1/src/program/Main.java
+++ seminar1/src/program/Main.java
@@ -1,10 +1,30 @@
 package program;
 
+import clase.Giraffe;
+import clase.Zebra;
+import clase.Zoo;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Zoo z=new Zoo();
+		Giraffe giraffe1 = new Giraffe("Girafa1");
+		Giraffe giraffe2 = new Giraffe("Girafa2");
+		
+		z.addAnimal(giraffe1);
+		z.addAnimal(giraffe2);
+		
+		z.feedAllAnimals();
+	
+	    Zebra z1 = new Zebra("z1");
+	    Zebra z2 = new Zebra("z2");
+	    
+	    z.addAnimal(z1);
+	    z.addAnimal(z2);
+	    
+	    z.feedAllAnimals();
 	}
 
 }

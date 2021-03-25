dc5974f8479553b3e2070bc014323a6e941845f4
grigo <grigo@LAPTOP-TKQJP1NQ>
2021-03-25 16:20:00 +0200

    SEMINAR 5 - simplefactory

---
+++ seminar5/src/ro/ase/cts/simplefactory/program/Main.java
@@ -0,0 +1,24 @@
+package ro.ase.cts.simplefactory.program;
+
+import ro.ase.cts.simplefactory.clase.FactoryPersonal;
+import ro.ase.cts.simplefactory.clase.Medic;
+import ro.ase.cts.simplefactory.clase.PersonalSpital;
+import ro.ase.cts.simplefactory.clase.TipPersonal;
+
+public class Main {
+	
+	public static void main(String[]args) {
+		try {
+		FactoryPersonal factory = new FactoryPersonal();
+		PersonalSpital medic= factory.getPersonal(TipPersonal.MEDIC, "Ana");
+		PersonalSpital asistent= factory.getPersonal(TipPersonal.ASISTENT, "Catalin");
+		
+		System.out.println(medic.toString());
+		System.out.println(asistent.toString());
+		
+	  }
+		catch(Exception e){
+		e.printStackTrace();
+	  }
+	}
+}

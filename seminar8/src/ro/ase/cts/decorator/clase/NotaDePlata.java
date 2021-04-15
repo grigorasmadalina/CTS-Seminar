58dd4bcddf734881e1b910ebc71b8c3cbb81daee
grigo <grigo@LAPTOP-TKQJP1NQ>
2021-04-15 16:29:40 +0300

    seminar 8 decorator

---
+++ seminar8/src/ro/ase/cts/decorator/clase/NotaDePlata.java
@@ -0,0 +1,14 @@
+package ro.ase.cts.decorator.clase;
+
+public class NotaDePlata implements NotaAbstracta{
+	private float suma;
+	
+	public void printeaza() {
+		System.out.println("Suma de plata este " + suma);
+	}
+	
+	public NotaDePlata(float suma) {
+		super();
+		this.suma=suma;
+	}
+}

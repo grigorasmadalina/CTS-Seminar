9afa991693c3716a74ba4601d131c6ca0cb86337
grigo <grigo@LAPTOP-TKQJP1NQ>
2021-05-06 16:13:42 +0300

    seminar 10 command partea I

---
+++ seminar10/src/ro/ase/cts/command/clase/ComandaRetragere.java
@@ -0,0 +1,17 @@
+  
+package ro.ase.cts.command.clase;
+
+public class ComandaRetragere extends Comanda{
+
+	public ComandaRetragere(ContBancar cont, float suma) {
+		super(cont, suma);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void executa() {
+		// TODO Auto-generated method stub
+		super.getCont().retragere(super.getSuma());
+	}
+
+}
